5241a93e2bd7fd136501c65b81e35bc20edebf09
tmaher0518 <36674845+tmaher0518@users.noreply.github.com>
2018-07-06 22:24:07 -0500

    Updated game.js

--- game.js
+++ game.js
@@ -1,97 +1,60 @@
-$( document ).ready(function(){
-// Generates random number to guess
-    var Random=Math.floor(Math.random()*102+19)
-    
-    // Display random number
-    $('#scoreToMatch').text(Random);
-    
-    //Generate random number for each crystal
-    var num1= Math.floor(Math.random()*12+1)
-    var num2= Math.floor(Math.random()*12+1)
-    var num3= Math.floor(Math.random()*12+1)
-    var num4= Math.floor(Math.random()*12+1)
-    
-    // Variables to keep track of wins, losses and total
-    var playerTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-    
-  
-  $('#wins').text(wins);
-  $('#losses').text(losses);
-  
-  // Reset game
-  function reset(){
-        Random=Math.floor(Math.random()*102+19);
-        console.log(Random)
-        $('#scoreToMatch').text(Random);
-        num1= Math.floor(Math.random()*12+1);
-        num2= Math.floor(Math.random()*12+1);
-        num3= Math.floor(Math.random()*12+1);
-        num4= Math.floor(Math.random()*12+1);
-        playerTotal= 0;
-        $('#totalScore').text(playerTotal);
-        } 
-  // Display wins
-  function woohoo(){
-  alert("Congrats! You won!");
-    wins++; 
-    $('#wins').text(wins);
-    reset();
-  }
-  // Display losses
-  function loser(){
-  alert ("Sorry! You lose!");
-    losses++;
-    $('#losses').text(losses);
-    reset()
-  }
-  // Clicking crystals
-    $('.red').on ('click', function(){
-      playerTotal = playerTotal + num1;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-            //Win & lose conditions
-          if (playerTotal == Random){
-            woohoo();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          }   
-    })  
-    $('.blue').on ('click', function(){
-      playerTotal = playerTotal + num2;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-          if (playerTotal == Random){
-            woohoo();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.yellow').on ('click', function(){
-      playerTotal = playerTotal + num3;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal);
-  
-            if (playerTotal == Random){
-            woohoo();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.green').on ('click', function(){
-      playerTotal = playerTotal + num4;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-        
-            if (playerTotal == Random){
-            woohoo();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          }
-    });   
-  });
+// Adding game variables 
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["./assets/images/blue.png", "./assets/images/green.png", "./assets/images/red.png", "./assets/images/yellow.png"];
+
+// On Click Functions
+
+	function randomTargetNumber () {
+		targetNumber = Math.floor(Math.random() * 102) + 19;
+	}
+
+	function resetCrystals () {
+		for (var i = 0; i < images.length; i++) {
+			var crystal = $("<img>");
+			crystal.addClass("crystal");	
+			crystal.attr("src", images[i]);
+			crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+			crystal.attr("height", "100");
+			$(".crystal-images").append(crystal);
+		}
+	}
+
+	function resetHTML () {
+		$(".target-number").html(targetNumber);
+		$(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+		$(".score-number").html(counter);
+		$(".crystal-images").empty();
+	}
+
+	function totalReset () {
+		randomTargetNumber ();
+		counter = 0;
+		resetHTML ();
+		resetCrystals ();
+	}
+
+
+	randomTargetNumber();
+	resetHTML ();
+	resetCrystals ();
+
+	// On click Functions
+	function crystalClick () {
+		//attr returns first value of selected html element
+		counter += parseInt($(this).attr("value"));
+		$(".score-number").html(counter);
+		if (counter == targetNumber) {
+			wins++;
+			totalReset();
+		}
+		else if (counter > targetNumber) {
+			losses++;
+			totalReset();
+		};
+	};
+
+	//Global crystalClick function
+	$(document).on("click", ".crystal", crystalClick);
